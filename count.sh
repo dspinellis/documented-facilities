060678621fda20f4d52f872dfdcc2bf9ff8112ac
Diomidis Spinellis <dds@aueb.gr>
2018-08-21 12:10:00 +0300

    Improve presentation and portability

--- count.sh
+++ count.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #
 
-for i in /usr/share/man/man* ; do
-  echo -n "$i "
+cd /usr/share/man
+for i man* ; do
+  printf "$i "
   ls $i | wc -l
 done
 
 echo Samples
-for i in /usr/share/man/man* ; do
-  echo -n $i
+for i in man* ; do
+  echo $i
   ls $i | head
   echo
 done
