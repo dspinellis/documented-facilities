21824f492aef269a0f208e0b7cd13f132ebdfa73
Diomidis Spinellis <dds@aueb.gr>
2018-08-21 11:44:55 +0300

    Add missing directory

--- count.sh
+++ count.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
 
-for i in /usr/share/man* ; do
+for i in /usr/share/man/man* ; do
   echo -n "$i "
   ls $i | wc -l
 done
 
 echo Samples
-for i in /usr/share/man* ; do
+for i in /usr/share/man/man* ; do
   echo -n $i
   ls $i | head
   echo
