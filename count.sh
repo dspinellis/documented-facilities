7da1b4d0114195c768da57cdda2af52e9c35b831
Diomidis Spinellis <dds@aueb.gr>
2018-11-25 11:50:45 +0200

    Remove $HOME references

--- count.sh
+++ count.sh
@@ -14,17 +14,15 @@ then
   . /etc/lsb-release
   # Obtain kernel sources
   git clone --depth=1 git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/$DISTRIB_CODENAME linux-src
-  echo Contents of HOME
-  ls $HOME
-
-  cd $HOME/linux-src
+  echo Contents of current directory
+  ls
 
   # Count supported device drivers
   printf "4p "
-  egrep -r $'^\t+.probe' drivers | wc -l
+  egrep -r $'^\t+.probe' linux-src/drivers | wc -l
 
   # Create man pages
-  scripts/kernel-doc -man $(find -name '*.c') 2>/dev/null | sudo $HOME/split-man.pl /usr/share/man/man9 2>/dev/null
+  linux-src/scripts/kernel-doc -man $(find linux-src -name '*.c') 2>/dev/null | sudo split-man.pl /usr/share/man/man9 2>/dev/null
 fi
 
 cd /usr/share/man
